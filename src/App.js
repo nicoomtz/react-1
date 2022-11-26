6c94c72baa41fc8adb024e27660f786cef3e4c9c
Nicolas Martinez <martineznicolas551@gmail.com>
2022-11-26 14:06:14 -0300

    layout updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import ContactoPage from './pages/ContactoPage';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import HomePage from './pages/HomePage';
+import Nav from './components/layout/Nav';
+import NosotrosPage from './pages/NosotrosPage';
+import NovedadesPage from './pages/NovedadesPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route
+            path='/'
+            element={<HomePage />}
+          />
+          <Route
+            path='/nosotros'
+            element={<NosotrosPage />}
+          />
+          <Route
+            path='/novedades'
+            element={<NovedadesPage />}
+          />
+          <Route
+            path='/contacto'
+            element={<ContactoPage />}
+          />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
